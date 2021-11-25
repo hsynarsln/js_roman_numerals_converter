3f12b388ccff192a4cd5d72e50694bf774fa6a43
hsynarsln <90923579+hsynarsln@users.noreply.github.com>
2021-11-25 15:17:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,19 +7,24 @@ const convertToRoman = num => {
   const decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
   const romanNumeral = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 
-  let romanized = '';
+  if (num > 0) {
+    let romanized = '';
 
-  for (let index = 0; index < decimalValue.length; index++) {
-    while (decimalValue[index] <= num) {
-      romanized += romanNumeral[index];
-      num -= decimalValue[index];
+    for (let index = 0; index < decimalValue.length; index++) {
+      while (decimalValue[index] <= num) {
+        romanized += romanNumeral[index];
+        num -= decimalValue[index];
+      }
     }
+    result.innerHTML = romanized;
+  } else {
+    result.innerHTML = `${num} is not a decimal number. Please enter a valid decimal number`;
   }
-  result.innerHTML = romanized;
 };
 
 submit.addEventListener('click', e => {
   e.preventDefault();
   convertToRoman(number.value);
   form.reset();
+  number.focus();
 });
